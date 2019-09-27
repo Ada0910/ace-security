5e7c838c53c5b1b4d128522d481ca83de669a4cb
Ada0910 <1355948107@qq.com>
2019-09-27 21:29:38 +0800

    commit

--- ace-control/ace-monitor/src/main/java/com/ada/monitor/MonitorApplication.java
+++ ace-control/ace-monitor/src/main/java/com/ada/monitor/MonitorApplication.java
@@ -1,5 +1,6 @@
 package com.ada.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
