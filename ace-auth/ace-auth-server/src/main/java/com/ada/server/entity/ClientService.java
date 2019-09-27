f61a1527d2f4b42c0fa7ac34279801c91a40d361
Ada0910 <1355948107@qq.com>
2019-09-27 23:32:09 +0800

    entity

---
+++ ace-auth/ace-auth-server/src/main/java/com/ada/server/entity/ClientService.java
@@ -0,0 +1,59 @@
+package com.ada.server.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * @author Ada
+ * @ClassName :ClientService
+ * @date 2019/9/27 23:11
+ * @Description:
+ */
+@Getter
+@Setter
+@Table(name = "auth_client_service")
+public class ClientService {
+    @Id
+    private Integer id;
+
+    @Column(name = "service_id")
+    private String serviceId;
+
+    @Column(name = "client_id")
+    private String clientId;
+
+    private String description;
+
+    @Column(name = "crt_time")
+    private Date crtTime;
+
+    @Column(name = "crt_user")
+    private String crtUser;
+
+    @Column(name = "crt_name")
+    private String crtName;
+
+    @Column(name = "crt_host")
+    private String crtHost;
+
+    private String attr1;
+
+    private String attr2;
+
+    private String attr3;
+
+    private String attr4;
+
+    private String attr5;
+
+    private String attr6;
+
+    private String attr7;
+
+    private String attr8;
+}
