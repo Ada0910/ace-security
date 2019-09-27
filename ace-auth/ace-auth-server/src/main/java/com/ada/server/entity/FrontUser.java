1a73bb952bb7ddd69ce22d5b073b4265bec47e2b
Ada0910 <l355948107@qq.com>
2019-09-27 17:45:55 +0800

    entity

---
+++ ace-auth/ace-auth-server/src/main/java/com/ada/server/entity/FrontUser.java
@@ -0,0 +1,22 @@
+package com.ada.server.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @ClassName:FrontUser
+ * @author: Ada
+ * @date 2019/09/2717:26
+ * @Description:
+ */
+@Getter
+@Setter
+public class FrontUser {
+    public String id ;
+    public String userName ;
+    public String name;
+    private String description;
+    private String image ;
+    //private List<PermissionInfo> menus;
+    //private List<PermissionInfo> elements;
+}
