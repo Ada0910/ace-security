6bc3b9a88b53719046bd1de0aafe153f648b61cc
Ada0910 <1355948107@qq.com>
2019-10-08 23:27:18 +0800

    client

---
+++ ace-common/src/main/java/com/ada/common/constant/RestCodeConstant.java
@@ -0,0 +1,12 @@
+package com.ada.common.constant;
+
+/**
+ * @author Ada
+ * @ClassName :RestCodeConstant
+ * @date 2019/10/8 23:10
+ * @Description:
+ */
+public class RestCodeConstant {
+    public static final int TOKEN_ERROR_CODE = 40101;
+    public static final int TOKEN_FORBIDDEN_CODE = 40301;
+}
