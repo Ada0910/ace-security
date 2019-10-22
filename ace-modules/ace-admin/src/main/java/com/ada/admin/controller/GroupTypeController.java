70f2e65519cfbd6074e37d4131b3223bf0d32afe
Ada0910 <l355948107@qq.com>
2019-10-22 17:35:17 +0800

    GroupType模块

---
+++ ace-modules/ace-admin/src/main/java/com/ada/admin/controller/GroupTypeController.java
@@ -0,0 +1,30 @@
+package com.ada.admin.controller;
+
+import com.ada.admin.GroupTypeBiz;
+import com.ada.admin.entity.GroupType;
+import com.ada.common.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName:GroupTypeController
+ * @author: Ada
+ * @date 2019/10/22 17:32
+ * @Description:
+ */
+@Controller
+@RequestMapping("groupType")
+public class GroupTypeController extends BaseController<GroupTypeBiz, GroupType> {
+//
+//    @RequestMapping(value = "/page",method = RequestMethod.GET)
+//    @ResponseBody
+//    public TableResultResponse<Object> page(@RequestParam(defaultValue = "10") int limit, @RequestParam(defaultValue = "1")int page, String name){
+//        Example example = new Example(GroupType.class);
+//        if(StringUtils.isNotBlank(name))
+//            example.createCriteria().andLike("name", "%" + name + "%");
+//        Page<Object> result = PageHelper.startPage(page, limit);
+//        baseBiz.selectByExample(example);
+//        return new TableResultResponse<Object>(result.getTotal(),result.getResult());
+//    }
+
+}
---
+++ ace-modules/ace-admin/src/main/java/com/ada/admin/mapper/GroupTypeMapper.java
@@ -0,0 +1,13 @@
+package com.ada.admin.mapper;
+
+import com.ada.admin.entity.GroupType;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @ClassName:GroupTypeMapper
+ * @author:Ada
+ * @date 2019/10/2217:34
+ * @Description:
+ */
+public interface GroupTypeMapper extends Mapper<GroupType> {
+}
