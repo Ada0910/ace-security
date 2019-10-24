543ed7d6467c649295644db229f9b362857cc6d9
Ada0910 <l355948107@qq.com>
2019-10-24 18:06:39 +0800

    配置拦截

--- ace-modules/ace-admin/src/main/java/com/ada/admin/config/WebConfig.java
+++ ace-modules/ace-admin/src/main/java/com/ada/admin/config/WebConfig.java
@@ -4,6 +4,8 @@ import com.ada.client.interceptor.ServiceAuthRestInterceptor;
 import com.ada.client.interceptor.UserAuthRestInterceptor;
 import com.ada.common.handler.GlobalExceptionHandler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,14 +18,20 @@ import java.util.Collections;
  * @date 2019/10/23 15:18
  * @Description:
  */
+@Configuration("admimWebConfig")
+@Primary
 public class WebConfig implements WebMvcConfigurer {
     @Bean
     GlobalExceptionHandler getGlobalExceptionHandler() {
         return new GlobalExceptionHandler();
     }
 
+    /**
+     * 拦截器配置
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        //添加拦截的路径
         registry.addInterceptor(getServiceAuthRestInterceptor()).
                 addPathPatterns(getIncludePathPatterns()).addPathPatterns("/api/user/validate");
         registry.addInterceptor(getUserAuthRestInterceptor()).
