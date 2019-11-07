da3e24b802580dbceef02eb65d8941103bd3b2b3
Ada0910 <l355948107@qq.com>
2019-11-07 18:02:22 +0800

    generator

---
+++ ace-modules/ace-generator/src/main/java/com/ada/generator/controller/GeneratorController.java
@@ -0,0 +1,57 @@
+package com.ada.generator.controller;
+
+import com.ada.common.response.TableResultResponse;
+import com.ada.generator.service.GeneratorService;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName:GeneratorController
+ * @author: Ada
+ * @date 2019/11/07 16:11
+ * @Description:
+ */
+@Controller
+@RequestMapping("/base/generator")
+public class GeneratorController {
+    @Autowired
+    private GeneratorService generatorService;
+
+    /**
+     * 列表
+     */
+    @ResponseBody
+    @RequestMapping("/page")
+    public TableResultResponse<Map<String, Object>> list(@RequestParam Map<String, Object> params) {
+        List<Map<String, Object>> result = generatorService.queryList(params);
+        int total = generatorService.queryTotal(params);
+        return new TableResultResponse<Map<String, Object>>(total, result);
+    }
+
+    /**
+     * 生成代码
+     */
+    @RequestMapping("/code")
+    public void code(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String[] tableNames = new String[]{};
+        String tables = request.getParameter("tables");
+        tableNames = tables.split(",");
+        byte[] data = generatorService.generatorCode(tableNames);
+        response.reset();
+        response.setHeader("Content-Disposition", "attachment; filename=\"ag-admin-code.zip\"");
+        response.addHeader("Content-Length", "" + data.length);
+        response.setContentType("application/octet-stream; charset=UTF-8");
+        IOUtils.write(data, response.getOutputStream());
+    }
+}
+
