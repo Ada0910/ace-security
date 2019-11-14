6d113b5abf19b57b79f81a1aec7bab1c2199a8a7
Ada0910 <l355948107@qq.com>
2019-11-14 17:56:42 +0800

    Log

---
+++ ace-gate/src/main/java/com/ada/gate/fallback/UserServiceFallback.java
@@ -0,0 +1,30 @@
+package com.ada.gate.fallback;
+
+import com.ada.api.vo.authority.PermissionInfo;
+import com.ada.gate.feign.IUserServiceFeign;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @ClassName:UserServiceFallback
+ * @author: Ada
+ * @date 2019/11/14 16:32
+ * @Description:
+ */
+@Service
+@Slf4j
+public class UserServiceFallback implements IUserServiceFeign {
+    @Override
+    public List<PermissionInfo> getPermissionByUsername(String userName) {
+        log.error("调用{}异常{}", "getPermissionByUsername", userName);
+        return null;
+    }
+
+    @Override
+    public List<PermissionInfo> getAllPermissionInfo() {
+        log.error("调用{}异常", "getPermissionByUsername");
+        return null;
+    }
+}
