0d8192884982f9c03c72597e8277b79f93566515
Ada0910 <1355948107@qq.com>
2019-12-02 23:43:40 +0800

    update

--- ace-gate/src/main/java/com/ada/gate/GateApplication.java
+++ ace-gate/src/main/java/com/ada/gate/GateApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableAceAuthClient
 @EnableFeignClients({"com.ada.client.feign", "com.ada.gate.feign"})
-@SpringBootApplication
 public class GateApplication {
     public static void main(String[] args) {
         DBLogUtil.getInstance().start();
