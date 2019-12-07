9552dcd8cad3c5dd69cc836aa11b4487137a1bc5
Ada0910 <1355948107@qq.com>
2019-12-07 23:10:52 +0800

    Update ServerApplication.java

--- ace-auth/ace-auth-server/src/main/java/com/ada/server/ServerApplication.java
+++ ace-auth/ace-auth-server/src/main/java/com/ada/server/ServerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @MapperScan("com.ada.server.mapper")
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableAutoConfiguration
+@SpringApplication
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
